c2fc3ec92827eda573ad11b555602262f444d3ab
Neptune430 <ofuluejohn39@gmail.com>
2025-07-18 22:34:57 +0100

    update

--- taskmanager/taskmanager/urls.py
+++ taskmanager/taskmanager/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.signup, name='signup'),  # Optionally name this route
+    path('signup/', views.signup, name='signup'),  # Optionally name this route
     path('login/', views.login, name='login'),  # Assuming you have a login view
     path('todopage/', views.todo, name='todo'),  # Add name='todo' here 
     path('edit_todo/<int:srno>', views.edit_todo, name='edit_todo'),  # Add name='edit_todo' here
